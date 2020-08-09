5e96edf5357ca9f00af51b9789fecdadc98cfc08
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-09 14:28:05 -0600

    Parsed strings into std::chrono::duration objects

--- src/Timecard.cpp
+++ src/Timecard.cpp
@@ -1,10 +1,24 @@
+#include <chrono>
 #include <iostream>
+
+auto ParseTimepoint(const std::string& timepoint)
+{
+    if (timepoint.size() != 4)
+    {
+        throw std::invalid_argument("Timepoint string must be 4 characters long");
+    }
+
+    const auto time = std::stoi(timepoint);
+    const auto minutes = std::chrono::minutes(time % 100);
+    const auto hours = std::chrono::hours((time - (time % 100)) / 100);
+
+    return hours + minutes;
+}
 
 int main(int argc, char* argv[])
 {
-    for (int i = 0; i < argc; ++i)
+    for (int i = 1; i < argc; ++i)
     {
-        std::cout << argv[i] << ' ';
+        std::cout << ParseTimepoint(argv[i]).count() << " minutes\n";
     }
-    std::cout << '\n';
 }
