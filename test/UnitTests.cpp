832c3bc76ffa3b359dc3c63cc04a15b205a94b90
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-10 22:05:25 -0600

    Added first tests for ParseTimepoint()

--- test/UnitTests.cpp
+++ test/UnitTests.cpp
@@ -1,8 +1,22 @@
+#include <ParseTimepoint.h>
+
 #include <gtest/gtest.h>
 
-TEST(Foo, Bar)
+TEST(ParseTimepoint, Garbage)
 {
-    EXPECT_TRUE(true);
+    EXPECT_THROW(ParseTimepoint(""), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint(" "), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("am"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("pm"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("asdf"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("just a plain ole string"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("0am"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("1pm"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("10"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("20"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("99"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("af13"), std::invalid_argument);
+    EXPECT_THROW(ParseTimepoint("1200tns"), std::invalid_argument);
 }
 
 int main(int argc, char* argv[])
