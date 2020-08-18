472e01882a2be840a3e82c6a199e9d01946fce66
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-17 23:42:21 -0600

    Moved argument checking into its own function

--- src/Timecard.cpp
+++ src/Timecard.cpp
@@ -1,3 +1,4 @@
+#include <CheckFlags.h>
 #include <ParseTimepoint.h>
 #include <VectorizeArguments.h>
 
@@ -10,20 +11,7 @@ int main(int argc, char* argv[])
 try
 {
     const auto args = VectorizeArguments(argc, argv);
-    if (args.size() > 1 and (args[1] == "-h" or args[1] == "--help"))
-    {
-        std::cout << "Usage: timecard <time1> <activity1> <time2> <activity2> <time3> <activityN> "
-                     "<timeN>\n\n";
-        std::cout
-            << "Times are formatted as 3 or 4 digits followed by either an 'am' or 'pm' suffix.\n";
-        return 0;
-    }
-
-    if (args.size() > 1 and (args[1] == "-v" or args[1] == "--version"))
-    {
-        std::cout << "Timecard version " << GIT_VERSION << '\n';
-        return 0;
-    }
+    CheckFlags(args);
 
     std::map<std::string, std::chrono::duration<double, std::ratio<3600>>> durations;
     for (size_t i = 2; i + 1 < args.size(); i += 2)
