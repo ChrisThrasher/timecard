ccfc1568552b611c2551d6a92c4c3dd2f8acb8d9
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-17 23:27:37 -0600

    Addressed clang-tidy error

--- include/VectorizeArguments.h
+++ include/VectorizeArguments.h
@@ -7,6 +7,7 @@ auto VectorizeArguments(const int argc, char* argv[]) -> std::vector<std::string
 {
     std::vector<std::string> arguments;
 
+    arguments.reserve(static_cast<size_t>(argc));
     for (int i = 0; i < argc; ++i)
     {
         arguments.push_back(std::string(argv[i]));
