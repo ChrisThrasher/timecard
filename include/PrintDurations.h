bd1fe69d13d64050dfdb3c1853eb50f0bf59965a
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-18 15:08:59 -0600

    Simplified how off times are parsed

--- include/PrintDurations.h
+++ include/PrintDurations.h
@@ -15,23 +15,21 @@ int LongestLabel(const DurationMap& durations)
     return static_cast<int>(longest_label);
 }
 
-void PrintDurations(const DurationMap& durations)
+void PrintDurations(DurationMap durations)
 {
     const char separator = ' ';
     const auto label_width = LongestLabel(durations) + 2;
     const auto duration_width = 4;
     std::cout << std::fixed << std::setprecision(1);
 
-    auto total = std::chrono::duration<double, std::ratio<3600>>(0);
-    auto off_time = std::chrono::duration<double, std::ratio<3600>>(0);
+    const auto off_time_it = durations.find("-");
+    const auto off_time =
+        (off_time_it != durations.end()) ? off_time_it->second : std::chrono::duration<double, std::ratio<3600>>(0);
+    durations.erase(off_time_it);
 
+    auto total = std::chrono::duration<double, std::ratio<3600>>(0);
     for (const auto& duration : durations)
     {
-        if (duration.first == "-")
-        {
-            off_time += duration.second;
-            continue;
-        }
         std::cout << std::left << std::setw(label_width) << std::setfill(separator) << duration.first;
         std::cout << std::right << std::setw(duration_width) << std::setfill(separator) << duration.second.count()
                   << " hours\n";
