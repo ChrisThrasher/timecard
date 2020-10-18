a623b93c89a8850dd849a168bffc230ae978905b
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-17 23:04:24 -0600

    Fixed bug in how flags are parsed

--- include/CheckFlags.h
+++ include/CheckFlags.h
@@ -7,14 +7,17 @@
 
 void CheckFlags(const std::vector<std::string>& args)
 {
-    if (args.size() > 1 and (args[1] == "-h" or args[1] == "--help"))
+    if (args.empty())
+        return;
+
+    const auto flag = args[0];
+    if (flag == "-h" or flag == "--help")
     {
         std::cout << "Usage: timecard <time1> <activity1> <time2> <activity2> <time3> <activityN> <timeN>\n\n";
         std::cout << "Times are formatted as 3 or 4 digits followed by either an 'am' or 'pm' suffix.\n";
         std::exit(EXIT_SUCCESS);
     }
-
-    if (args.size() > 1 and (args[1] == "-v" or args[1] == "--version"))
+    if (flag == "-v" or flag == "--version")
     {
         std::cout << "Timecard version " << GIT_VERSION << '\n';
         std::exit(EXIT_SUCCESS);
