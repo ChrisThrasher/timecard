1c357a0c5472daa1d3949fd49698d2b6f0cd0d54
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-18 00:14:37 -0600

    Added total duration printout

--- include/PrintDurations.h
+++ include/PrintDurations.h
@@ -7,11 +7,14 @@
 
 void PrintDurations(const DurationMap& durations)
 {
+    auto total_duration = std::chrono::duration<double, std::ratio<3600>>(0);
     std::cout << std::fixed << std::setprecision(1);
     for (const auto& duration : durations)
     {
         if (duration.first == "-")
             continue;
         std::cout << duration.first << ": " << duration.second.count() << " hours\n";
+        total_duration += duration.second;
     }
+    std::cout << "\nTotal: " << total_duration.count() << " hours\n";
 }
