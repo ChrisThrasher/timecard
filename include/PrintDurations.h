7cbe5d9e4cd948361b1959fed076965b3c660501
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-18 00:16:41 -0600

    Added time off printout

--- include/PrintDurations.h
+++ include/PrintDurations.h
@@ -8,13 +8,17 @@
 void PrintDurations(const DurationMap& durations)
 {
     auto total_duration = std::chrono::duration<double, std::ratio<3600>>(0);
+    auto off_time = std::chrono::duration<double, std::ratio<3600>>(0);
     std::cout << std::fixed << std::setprecision(1);
     for (const auto& duration : durations)
     {
         if (duration.first == "-")
+        {
+            off_time += duration.second;
             continue;
+        }
         std::cout << duration.first << ": " << duration.second.count() << " hours\n";
         total_duration += duration.second;
     }
-    std::cout << "\nTotal: " << total_duration.count() << " hours\n";
+    std::cout << "\nTotal: " << total_duration.count() << " hours (" << off_time.count() << " hours off)\n";
 }
