b8730760924b7274c5b63f1e8a644ba23ed20f35
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-19 22:24:59 -0600

    Simplified VectorizeArguments implementation
    Shoutout to a coworker for giving me this sweet one-liner.

--- include/VectorizeArguments.h
+++ include/VectorizeArguments.h
@@ -5,13 +5,5 @@
 
 auto VectorizeArguments(const int argc, char* argv[]) -> std::vector<std::string>
 {
-    std::vector<std::string> arguments;
-
-    arguments.reserve(static_cast<size_t>(argc));
-    for (int i = 1; i < argc; ++i) // Skip first element ignoring name of program
-    {
-        arguments.push_back(std::string(argv[i]));
-    }
-
-    return arguments;
+    return std::vector<std::string>(argv + 1, argv + argc);
 }
