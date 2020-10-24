addd5d140d1587a7275f664cae572f51837f8f15
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-23 23:10:16 -0600

    Used multiline string literal for help text

--- include/CheckFlags.h
+++ include/CheckFlags.h
@@ -5,6 +5,12 @@
 #include <string>
 #include <vector>
 
+static constexpr auto help_text = R"(Usage: timecard <time1> <activity1> <time2> <activity2> <time3> <activityN> <timeN>
+
+Times are formatted as 3 or 4 digits followed by either an 'am' or 'pm' suffix.
+Any activities named "-" will be ignored.
+)";
+
 void CheckFlags(const std::vector<std::string>& args)
 {
     if (args.empty())
@@ -13,9 +19,7 @@ void CheckFlags(const std::vector<std::string>& args)
     const auto flag = args[0];
     if (flag == "-h" or flag == "--help")
     {
-        std::cout << "Usage: timecard <time1> <activity1> <time2> <activity2> <time3> <activityN> <timeN>\n\n";
-        std::cout << "Times are formatted as 3 or 4 digits followed by either an 'am' or 'pm' suffix.\n";
-        std::cout << "Any activities named \"-\" will be ignored.\n";
+        std::cout << help_text;
         std::exit(EXIT_SUCCESS);
     }
     if (flag == "-v" or flag == "--version")
