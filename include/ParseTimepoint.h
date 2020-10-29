d528f71c1816d1d0c52ae153bf310da63e0e4800
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-28 22:09:45 -0600

    Removed extraneous braces

--- include/ParseTimepoint.h
+++ include/ParseTimepoint.h
@@ -22,22 +22,16 @@ try
 
     const auto modified_timepoint = timepoint.substr(0, timepoint.size() - 2);
     if (modified_timepoint.size() > 4 or modified_timepoint.size() < 3)
-    {
         throw std::invalid_argument("Must contain 3 or 4 consecutive numbers.");
-    }
 
     const auto time = std::stoi(modified_timepoint);
     const auto minutes = time % 100;
     if (minutes >= 60 or minutes < 0)
-    {
         throw std::invalid_argument("Minutes not in range [0, 59].");
-    }
 
     const auto raw_hours = (time - minutes) / 100;
     if (raw_hours > 12 or raw_hours <= 0)
-    {
         throw std::invalid_argument("Hours not in range [1, 12].");
-    }
 
     const auto hours = raw_hours % 12 + (is_pm ? 12 : 0);
     return std::chrono::hours(hours) + std::chrono::minutes(minutes);
