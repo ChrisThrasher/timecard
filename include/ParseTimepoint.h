6f7da32c7a695c5d43a57546ae838d1a315f549e
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-10-28 22:23:47 -0600

    Removed 'or equals' operators to improve readability

--- include/ParseTimepoint.h
+++ include/ParseTimepoint.h
@@ -22,11 +22,11 @@ try
 
     const auto time = std::stoi(timepoint.substr(0, timepoint.size() - 2));
     const auto minutes = time % 100;
-    if (minutes >= 60 or minutes < 0)
+    if (minutes > 59 or minutes < 0)
         throw std::invalid_argument("Minutes not in range [0, 59].");
 
     const auto raw_hours = (time - minutes) / 100;
-    if (raw_hours > 12 or raw_hours <= 0)
+    if (raw_hours > 12 or raw_hours < 1)
         throw std::invalid_argument("Hours not in range [1, 12].");
 
     const auto hours = raw_hours % 12 + (is_pm ? 12 : 0);
