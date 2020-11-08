b8b3a997e68b5f9ae3a1109387951a77d61ee353
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-08 16:36:54 -0700

    Used regex for timepoint verification

--- include/ParseTimepoint.h
+++ include/ParseTimepoint.h
@@ -15,10 +15,9 @@ bool HasSuffix(const std::string& timepoint, const std::string& suffix)
 auto ParseTimepoint(const std::string& timepoint)
 try
 {
-    const auto is_am = HasSuffix(timepoint, "am");
-    const auto is_pm = HasSuffix(timepoint, "pm");
-    if (not is_am and not is_pm)
-        throw std::invalid_argument("No am or pm suffix");
+    constexpr auto timepoint_regex = "\\d{3,4}(a|p)m";
+    if (not std::regex_match(timepoint, std::regex(timepoint_regex)))
+        throw std::invalid_argument(std::string("Did not match regex: ") + timepoint_regex);
 
     const auto time = std::stoi(timepoint.substr(0, timepoint.size() - 2));
     const auto minutes = time % 100;
@@ -29,7 +28,7 @@ try
     if (raw_hours > 12 or raw_hours < 1)
         throw std::invalid_argument("Hours (" + std::to_string(raw_hours) + ") not in range [1, 12]");
 
-    const auto hours = raw_hours % 12 + (is_pm ? 12 : 0);
+    const auto hours = raw_hours % 12 + (HasSuffix(timepoint, "pm") ? 12 : 0);
     return std::chrono::hours(hours) + std::chrono::minutes(minutes);
 }
 catch (const std::exception& ex)
