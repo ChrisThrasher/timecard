9452a99853d3b33a095837788250712ea2e93e90
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-08 16:53:52 -0700

    Used regex to verify minutes

--- include/ParseTimepoint.h
+++ include/ParseTimepoint.h
@@ -19,14 +19,12 @@ int AmPmOffset(const std::string& timepoint)
 auto ParseTimepoint(const std::string& timepoint)
 try
 {
-    constexpr auto timepoint_regex = "\\d{3,4}(a|p)m";
+    constexpr auto timepoint_regex = "\\d{1,2}[0-5][0-9](a|p)m";
     if (not std::regex_match(timepoint, std::regex(timepoint_regex)))
         throw std::invalid_argument(std::string("Did not match regex: ") + timepoint_regex);
 
     const auto time = std::stoi(timepoint.substr(0, timepoint.size() - 2));
     const auto minutes = time % 100;
-    if (minutes > 59 or minutes < 0)
-        throw std::invalid_argument("Minutes (" + std::to_string(minutes) + ") not in range [0, 59]");
 
     const auto raw_hours = (time - minutes) / 100;
     if (raw_hours > 12 or raw_hours < 1)
