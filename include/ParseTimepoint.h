04598226718ebc3a8f07724d58504d4c9c641df0
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-08 17:45:39 -0700

    Changed required timepoint formatting to include colon

--- include/ParseTimepoint.h
+++ include/ParseTimepoint.h
@@ -18,10 +18,10 @@ int AmPmOffset(const std::string& timepoint)
 
 auto ParseTimepoint(const std::string& timepoint)
 {
-    if (not std::regex_match(timepoint, std::regex("([1][0-2]|[1-9])[0-5][0-9](a|p)m")))
+    if (not std::regex_match(timepoint, std::regex("([1][0-2]|[1-9]):[0-5][0-9](a|p)m")))
         throw std::invalid_argument("Failed to parse \"" + timepoint + "\"");
 
-    const auto hours = std::stoi(timepoint.substr(0, timepoint.size() - 4)) % 12 + AmPmOffset(timepoint);
+    const auto hours = std::stoi(timepoint.substr(0, timepoint.size() - 5)) % 12 + AmPmOffset(timepoint);
     const auto minutes = std::stoi(timepoint.substr(timepoint.size() - 4, 2));
 
     return std::chrono::hours(hours) + std::chrono::minutes(minutes);
