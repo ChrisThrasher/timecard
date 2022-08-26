8d7f1a11913b2adfd90d0b5a77d06ef8f08ecef1
Chris Thrasher <chrisjthrasher@gmail.com>
2022-08-26 17:06:48 -0600

    Upgrade to Catch2 v3

--- tests/UnitTests.cpp
+++ tests/UnitTests.cpp
@@ -3,8 +3,7 @@
 #include <timecard/FormatDurations.hpp>
 #include <timecard/ParseTimepoint.hpp>
 
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("Program version")
 {
