76324b3246be0039c025570cef085b5981fa7437
Chris Thrasher <chrisjthrasher@gmail.com>
2022-10-16 09:29:37 -0600

    Exclude 3rd party sources from compile commands database

--- cmake/Findargon.cmake
+++ cmake/Findargon.cmake
@@ -4,6 +4,6 @@ FetchContent_Declare(argon
     GIT_REPOSITORY https://github.com/ChrisThrasher/argon.git
     GIT_TAG v${argon_FIND_VERSION})
 FetchContent_MakeAvailable(argon)
-set_target_properties(argon PROPERTIES COMPILE_OPTIONS "")
+set_target_properties(argon PROPERTIES COMPILE_OPTIONS "" EXPORT_COMPILE_COMMANDS OFF)
 get_target_property(ARGON_INCLUDE_DIRS argon INTERFACE_INCLUDE_DIRECTORIES)
 target_include_directories(argon SYSTEM INTERFACE ${ARGON_INCLUDE_DIRS})
